49f4b75005ac7f147bad79b8e8470068ad22c108
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-07-31 01:26:34 +0000

    1-4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,18 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  const defaultGrade = { grade: 'N/A' };
+
+  if (Array.isArray(students)) {
+    return students
+      .filter((student) => student.location === city)
+      .map((student) => {
+        const studentGrade = newGrades.find((grade) => grade.studentId === student.id) || defaultGrade;
+        return {
+          id: student.id,
+          firstName: student.firstName,
+          location: student.location,
+          grade: studentGrade.grade,
+        };
+      });
+  }
+  return [];
+}
